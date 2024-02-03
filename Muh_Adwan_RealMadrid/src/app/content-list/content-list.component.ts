edceb76a9fc7f8accb9d9ddcadf1b610f912ea80
MuhammedAdwan <133061822+MuhammedAdwan@users.noreply.github.com>
2024-02-03 16:27:04 -0500

    changing the contents to have 2 types instead of one

--- Muh_Adwan_RealMadrid/src/app/content-list/content-list.component.ts
+++ Muh_Adwan_RealMadrid/src/app/content-list/content-list.component.ts
@@ -13,7 +13,7 @@ export class ContentListComponent {
       description: 'Rodrygo Scoring the winning come back goal for Real Madrid against Manchester City',
       creator: 'The Guardian',
       imgURL: 'https://i.guim.co.uk/img/media/7b8b782959e0766bef222ea898a72b5f69aac4ba/0_290_8640_5184/master/8640.jpg?width=700&quality=85&auto=format&fit=max&s=9f6b7c4acfe317d2f2ce99fbe4031c14',
-      type: 'Pic1',
+      type: ['type1', 'type2'],
       tags: ['Rodrygo', 'Brazil'] 
     },
     {
@@ -22,7 +22,7 @@ export class ContentListComponent {
       description: 'Vinicius Jr. Celevrating after scoring a winning goal in the champions league final',
       creator: 'YouTube',
       imgURL: 'https://assets-cms.thescore.com/uploads/image/file/515973/w640xh480_GettyImages-1240963545.jpg?ts=1653772511',
-      type: 'Pic 2',
+      type: ['type1', 'type2'],
       tags: ['Vinicius', 'Brazil']
     },
     {
@@ -31,7 +31,7 @@ export class ContentListComponent {
       description: 'Luka Modric celebrating 14th Champion league for real madrid and his 5th one',
       creator: 'BBC',
       imgURL: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQdLYDIoo2nc9lK7WhFsWemQkp9OJ-PNAM7ig&usqp=CAU',
-      type: 'Pic 3',
+      type: ['type1', 'type2'],
       tags: ['Luka', 'Croatia']
     },
 
@@ -39,9 +39,9 @@ export class ContentListComponent {
       id: 4,
         title: 'Dani Carvajal',
         description: 'Dani Celebrating a winning comeback goal for Real Madrid Against Almaria ',
-        creator: 'Creator 6',
-        imgURL: 'https://s.france24.com/media/display/6db04a66-b883-11ee-a5fb-005056a90284/w:980/p:16x9/44ee04d5bb48c9b65a63c3e19daa14eb4dfa415d.jpg',
-        type: 'pic 4',
+        creator: 'The Sun',
+        imgURL: 'https://media.video-cdn.espn.com/motion/2024/0110/dm_240110_carvajal_goal/dm_240110_carvajal_goal.jpg',
+        type: ['type1', 'type2'],
         tags: ['Carvajal', 'Spain']
     },
 
@@ -51,7 +51,7 @@ export class ContentListComponent {
       description: 'Jude Bellingham celebrating his first goal for Real Madrid in Champions League',
       creator: 'The Guardian',
       imgURL: 'https://i.guim.co.uk/img/media/fdfe1d3ec93af3a1e505ab888b169912bcb53ef7/71_123_3377_2026/master/3377.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=156e03059aba73583a481fbd558c5e66',
-      type: 'Pic 5',
+      type: ['type1', 'type2'],
       tags: ['Jude', 'England']
   },
   {
@@ -60,8 +60,18 @@ export class ContentListComponent {
       description: 'Carlos Casemiro holding his 5th Champions League trophy',
       creator: 'Sky Sport',
       imgURL: 'https://e0.365dm.com/22/08/768x432/skysports-casemiro-real-madrid_5865117.jpg?20220815085230',
-      type: 'Type 6',
+      type: ['type1', 'type2'],
       tags: ['Casemiro', 'Brazil']
+  },
+
+  {
+    id: 7,
+      title: 'Zinedin Zidan',
+      description: 'Zizou scoring the winning goal which helped Real Madrid to be the champions for the 9th time',
+      creator: 'SportBible',
+      imgURL: 'https://images.ladbible.com/resize?type=jpeg&quality=70&width=720&fit=contain&gravity=null&url=https://s3-images.sportbible.com/s3/content/0de5be99591978e798955a76ce025ef6.png',
+      type: ['type1', 'type2'],
+      tags: ['Zidan', 'France']
   }
 
 ]
